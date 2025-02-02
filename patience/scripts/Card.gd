fa7ef81ea4ecd23d037e8f460e8733119ab0c207
pmaitland <pmaitland98@gmail.com>
2025-02-02 22:52:52 +0000

    allowed cards to be clicked and dragged

--- patience/scripts/Card.gd
+++ patience/scripts/Card.gd
@@ -10,14 +10,32 @@ extends Node2D
 
 var face_up: bool = true
 
+var last_mouse_position: Vector2 = Vector2.ZERO
 var has_mouse: bool = false
+var being_dragged: bool = false
 
 func _ready() -> void:
 	value_label.text = str(value)
 	suit_label.text = str(suit)
 
 func _process(_delta: float) -> void:
-	pass
+	var current_mouse_position = get_global_mouse_position()
+	
+	if has_mouse and !Globals.mouse_has_card and Input.is_mouse_button_pressed(MOUSE_BUTTON_LEFT):
+		being_dragged = true
+		scale = Vector2(1.2, 1.2)
+		Globals.mouse_has_card = true
+	
+	if being_dragged:
+		if Input.is_mouse_button_pressed(MOUSE_BUTTON_LEFT):
+			global_position.x += current_mouse_position.x - last_mouse_position.x
+			global_position.y += current_mouse_position.y - last_mouse_position.y
+		else:
+			being_dragged = false
+			scale = Vector2(1, 1)
+			Globals.mouse_has_card = false
+		
+	last_mouse_position = current_mouse_position
 	
 func flip() -> void:
 	face_up = !face_up
@@ -31,3 +49,9 @@ func flip() -> void:
 		value_label.visible = false
 		suit_label.visible = false
 		back_sprite.visible = true
+
+func _on_area_2d_mouse_entered() -> void:
+	has_mouse = true
+
+func _on_area_2d_mouse_exited() -> void:
+	has_mouse = false
---
+++ patience/scripts/Globals.gd
@@ -0,0 +1,3 @@
+extends Node
+
+var mouse_has_card: bool = false
